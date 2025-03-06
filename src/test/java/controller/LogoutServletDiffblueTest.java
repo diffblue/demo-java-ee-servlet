ad458a6d434be2d7ac7fbbe7cc1fd37d59906a37
Animesh <12829119+SirAnimesh@users.noreply.github.com>
2025-03-06 12:31:44 +0000

    Out of the box coverage jumps to 36.4% with spring-test

--- src/test/java/controller/LogoutServletDiffblueTest.java
+++ src/test/java/controller/LogoutServletDiffblueTest.java
@@ -1,17 +1,21 @@
 package controller;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import com.diffblue.cover.annotations.MaintainedByDiffblue;
 import com.diffblue.cover.annotations.MethodsUnderTest;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Set;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponseWrapper;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
 
 public class LogoutServletDiffblueTest {
   /**
@@ -30,60 +34,124 @@ public class LogoutServletDiffblueTest {
   /**
    * Test {@link LogoutServlet#doGet(HttpServletRequest, HttpServletResponse)}.
    * <ul>
-   *   <li>When {@link HttpServletRequestWrapper#HttpServletRequestWrapper(HttpServletRequest)} with request is {@code null}.</li>
+   *   <li>Given {@link MockHttpSession#MockHttpSession()}.</li>
    * </ul>
    * <p>
    * Method under test: {@link LogoutServlet#doGet(HttpServletRequest, HttpServletResponse)}
    */
   @Test
-  @Ignore("TODO: Complete this test")
   @Category(MaintainedByDiffblue.class)
   @MethodsUnderTest({"void LogoutServlet.doGet(HttpServletRequest, HttpServletResponse)"})
-  public void testDoGet_whenHttpServletRequestWrapperWithRequestIsNull() throws IOException, ServletException {
-    // TODO: Diffblue Cover was only able to create a partial test for this method:
-    //   Reason: No inputs found that don't throw a trivial exception.
-    //   Diffblue Cover tried to run the arrange/act section, but the method under
-    //   test threw
-    //   java.lang.IllegalArgumentException: Request cannot be null
-    //       at javax.servlet.ServletRequestWrapper.<init>(ServletRequestWrapper.java:50)
-    //       at javax.servlet.http.HttpServletRequestWrapper.<init>(HttpServletRequestWrapper.java:47)
-    //   See https://diff.blue/R013 to resolve this issue.
-
+  public void testDoGet_givenMockHttpSession() throws IOException, ServletException {
     // Arrange
     LogoutServlet logoutServlet = new LogoutServlet();
-    HttpServletRequestWrapper request = new HttpServletRequestWrapper(null);
+
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    request.setSession(new MockHttpSession());
+    MockHttpServletResponse response = new MockHttpServletResponse();
 
     // Act
-    logoutServlet.doGet(request, new HttpServletResponseWrapper(null));
+    logoutServlet.doGet(request, response);
+
+    // Assert
+    Collection<String> headerNames = response.getHeaderNames();
+    assertEquals(1, headerNames.size());
+    assertTrue(headerNames instanceof Set);
+    assertEquals("login", response.getRedirectedUrl());
+    assertEquals(302, response.getStatus());
+    assertTrue(headerNames.contains("Location"));
+    assertTrue(response.isCommitted());
+  }
+
+  /**
+   * Test {@link LogoutServlet#doGet(HttpServletRequest, HttpServletResponse)}.
+   * <ul>
+   *   <li>Then {@link MockHttpServletResponse} (default constructor) HeaderNames size is one.</li>
+   * </ul>
+   * <p>
+   * Method under test: {@link LogoutServlet#doGet(HttpServletRequest, HttpServletResponse)}
+   */
+  @Test
+  @Category(MaintainedByDiffblue.class)
+  @MethodsUnderTest({"void LogoutServlet.doGet(HttpServletRequest, HttpServletResponse)"})
+  public void testDoGet_thenMockHttpServletResponseHeaderNamesSizeIsOne() throws IOException, ServletException {
+    // Arrange
+    LogoutServlet logoutServlet = new LogoutServlet();
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    MockHttpServletResponse response = new MockHttpServletResponse();
+
+    // Act
+    logoutServlet.doGet(request, response);
+
+    // Assert
+    Collection<String> headerNames = response.getHeaderNames();
+    assertEquals(1, headerNames.size());
+    assertTrue(headerNames instanceof Set);
+    assertEquals("login", response.getRedirectedUrl());
+    assertEquals(302, response.getStatus());
+    assertTrue(headerNames.contains("Location"));
+    assertTrue(response.isCommitted());
   }
 
   /**
    * Test {@link LogoutServlet#doPost(HttpServletRequest, HttpServletResponse)}.
    * <ul>
-   *   <li>When {@link HttpServletRequestWrapper#HttpServletRequestWrapper(HttpServletRequest)} with request is {@code null}.</li>
+   *   <li>Given {@link MockHttpSession#MockHttpSession()}.</li>
    * </ul>
    * <p>
    * Method under test: {@link LogoutServlet#doPost(HttpServletRequest, HttpServletResponse)}
    */
   @Test
-  @Ignore("TODO: Complete this test")
   @Category(MaintainedByDiffblue.class)
   @MethodsUnderTest({"void LogoutServlet.doPost(HttpServletRequest, HttpServletResponse)"})
-  public void testDoPost_whenHttpServletRequestWrapperWithRequestIsNull() throws IOException, ServletException {
-    // TODO: Diffblue Cover was only able to create a partial test for this method:
-    //   Reason: No inputs found that don't throw a trivial exception.
-    //   Diffblue Cover tried to run the arrange/act section, but the method under
-    //   test threw
-    //   java.lang.IllegalArgumentException: Request cannot be null
-    //       at javax.servlet.ServletRequestWrapper.<init>(ServletRequestWrapper.java:50)
-    //       at javax.servlet.http.HttpServletRequestWrapper.<init>(HttpServletRequestWrapper.java:47)
-    //   See https://diff.blue/R013 to resolve this issue.
-
+  public void testDoPost_givenMockHttpSession() throws IOException, ServletException {
     // Arrange
     LogoutServlet logoutServlet = new LogoutServlet();
-    HttpServletRequestWrapper request = new HttpServletRequestWrapper(null);
+
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    request.setSession(new MockHttpSession());
+    MockHttpServletResponse response = new MockHttpServletResponse();
 
     // Act
-    logoutServlet.doPost(request, new HttpServletResponseWrapper(null));
+    logoutServlet.doPost(request, response);
+
+    // Assert
+    Collection<String> headerNames = response.getHeaderNames();
+    assertEquals(1, headerNames.size());
+    assertTrue(headerNames instanceof Set);
+    assertEquals("login", response.getRedirectedUrl());
+    assertEquals(302, response.getStatus());
+    assertTrue(headerNames.contains("Location"));
+    assertTrue(response.isCommitted());
+  }
+
+  /**
+   * Test {@link LogoutServlet#doPost(HttpServletRequest, HttpServletResponse)}.
+   * <ul>
+   *   <li>Then {@link MockHttpServletResponse} (default constructor) HeaderNames size is one.</li>
+   * </ul>
+   * <p>
+   * Method under test: {@link LogoutServlet#doPost(HttpServletRequest, HttpServletResponse)}
+   */
+  @Test
+  @Category(MaintainedByDiffblue.class)
+  @MethodsUnderTest({"void LogoutServlet.doPost(HttpServletRequest, HttpServletResponse)"})
+  public void testDoPost_thenMockHttpServletResponseHeaderNamesSizeIsOne() throws IOException, ServletException {
+    // Arrange
+    LogoutServlet logoutServlet = new LogoutServlet();
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    MockHttpServletResponse response = new MockHttpServletResponse();
+
+    // Act
+    logoutServlet.doPost(request, response);
+
+    // Assert
+    Collection<String> headerNames = response.getHeaderNames();
+    assertEquals(1, headerNames.size());
+    assertTrue(headerNames instanceof Set);
+    assertEquals("login", response.getRedirectedUrl());
+    assertEquals(302, response.getStatus());
+    assertTrue(headerNames.contains("Location"));
+    assertTrue(response.isCommitted());
   }
 }
--- src/test/java/controller/ViewMessageServletDiffblueTest.java
+++ src/test/java/controller/ViewMessageServletDiffblueTest.java
@@ -1,17 +1,21 @@
 package controller;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import com.diffblue.cover.annotations.MaintainedByDiffblue;
 import com.diffblue.cover.annotations.MethodsUnderTest;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Set;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponseWrapper;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
 
 public class ViewMessageServletDiffblueTest {
   /**
@@ -30,60 +34,124 @@ public class ViewMessageServletDiffblueTest {
   /**
    * Test {@link ViewMessageServlet#doGet(HttpServletRequest, HttpServletResponse)}.
    * <ul>
-   *   <li>When {@link HttpServletRequestWrapper#HttpServletRequestWrapper(HttpServletRequest)} with request is {@code null}.</li>
+   *   <li>Given {@link MockHttpSession#MockHttpSession()}.</li>
    * </ul>
    * <p>
    * Method under test: {@link ViewMessageServlet#doGet(HttpServletRequest, HttpServletResponse)}
    */
   @Test
-  @Ignore("TODO: Complete this test")
   @Category(MaintainedByDiffblue.class)
   @MethodsUnderTest({"void ViewMessageServlet.doGet(HttpServletRequest, HttpServletResponse)"})
-  public void testDoGet_whenHttpServletRequestWrapperWithRequestIsNull() throws IOException, ServletException {
-    // TODO: Diffblue Cover was only able to create a partial test for this method:
-    //   Reason: No inputs found that don't throw a trivial exception.
-    //   Diffblue Cover tried to run the arrange/act section, but the method under
-    //   test threw
-    //   java.lang.IllegalArgumentException: Request cannot be null
-    //       at javax.servlet.ServletRequestWrapper.<init>(ServletRequestWrapper.java:50)
-    //       at javax.servlet.http.HttpServletRequestWrapper.<init>(HttpServletRequestWrapper.java:47)
-    //   See https://diff.blue/R013 to resolve this issue.
-
+  public void testDoGet_givenMockHttpSession() throws IOException, ServletException {
     // Arrange
     ViewMessageServlet viewMessageServlet = new ViewMessageServlet();
-    HttpServletRequestWrapper request = new HttpServletRequestWrapper(null);
+
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    request.setSession(new MockHttpSession());
+    MockHttpServletResponse response = new MockHttpServletResponse();
 
     // Act
-    viewMessageServlet.doGet(request, new HttpServletResponseWrapper(null));
+    viewMessageServlet.doGet(request, response);
+
+    // Assert
+    Collection<String> headerNames = response.getHeaderNames();
+    assertEquals(1, headerNames.size());
+    assertTrue(headerNames instanceof Set);
+    assertEquals("login", response.getRedirectedUrl());
+    assertEquals(302, response.getStatus());
+    assertTrue(headerNames.contains("Location"));
+    assertTrue(response.isCommitted());
+  }
+
+  /**
+   * Test {@link ViewMessageServlet#doGet(HttpServletRequest, HttpServletResponse)}.
+   * <ul>
+   *   <li>Then {@link MockHttpServletResponse} (default constructor) HeaderNames size is one.</li>
+   * </ul>
+   * <p>
+   * Method under test: {@link ViewMessageServlet#doGet(HttpServletRequest, HttpServletResponse)}
+   */
+  @Test
+  @Category(MaintainedByDiffblue.class)
+  @MethodsUnderTest({"void ViewMessageServlet.doGet(HttpServletRequest, HttpServletResponse)"})
+  public void testDoGet_thenMockHttpServletResponseHeaderNamesSizeIsOne() throws IOException, ServletException {
+    // Arrange
+    ViewMessageServlet viewMessageServlet = new ViewMessageServlet();
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    MockHttpServletResponse response = new MockHttpServletResponse();
+
+    // Act
+    viewMessageServlet.doGet(request, response);
+
+    // Assert
+    Collection<String> headerNames = response.getHeaderNames();
+    assertEquals(1, headerNames.size());
+    assertTrue(headerNames instanceof Set);
+    assertEquals("login", response.getRedirectedUrl());
+    assertEquals(302, response.getStatus());
+    assertTrue(headerNames.contains("Location"));
+    assertTrue(response.isCommitted());
   }
 
   /**
    * Test {@link ViewMessageServlet#doPost(HttpServletRequest, HttpServletResponse)}.
    * <ul>
-   *   <li>When {@link HttpServletRequestWrapper#HttpServletRequestWrapper(HttpServletRequest)} with request is {@code null}.</li>
+   *   <li>Given {@link MockHttpSession#MockHttpSession()}.</li>
    * </ul>
    * <p>
    * Method under test: {@link ViewMessageServlet#doPost(HttpServletRequest, HttpServletResponse)}
    */
   @Test
-  @Ignore("TODO: Complete this test")
   @Category(MaintainedByDiffblue.class)
   @MethodsUnderTest({"void ViewMessageServlet.doPost(HttpServletRequest, HttpServletResponse)"})
-  public void testDoPost_whenHttpServletRequestWrapperWithRequestIsNull() throws IOException, ServletException {
-    // TODO: Diffblue Cover was only able to create a partial test for this method:
-    //   Reason: No inputs found that don't throw a trivial exception.
-    //   Diffblue Cover tried to run the arrange/act section, but the method under
-    //   test threw
-    //   java.lang.IllegalArgumentException: Request cannot be null
-    //       at javax.servlet.ServletRequestWrapper.<init>(ServletRequestWrapper.java:50)
-    //       at javax.servlet.http.HttpServletRequestWrapper.<init>(HttpServletRequestWrapper.java:47)
-    //   See https://diff.blue/R013 to resolve this issue.
-
+  public void testDoPost_givenMockHttpSession() throws IOException, ServletException {
     // Arrange
     ViewMessageServlet viewMessageServlet = new ViewMessageServlet();
-    HttpServletRequestWrapper request = new HttpServletRequestWrapper(null);
+
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    request.setSession(new MockHttpSession());
+    MockHttpServletResponse response = new MockHttpServletResponse();
 
     // Act
-    viewMessageServlet.doPost(request, new HttpServletResponseWrapper(null));
+    viewMessageServlet.doPost(request, response);
+
+    // Assert
+    Collection<String> headerNames = response.getHeaderNames();
+    assertEquals(1, headerNames.size());
+    assertTrue(headerNames instanceof Set);
+    assertEquals("login", response.getRedirectedUrl());
+    assertEquals(302, response.getStatus());
+    assertTrue(headerNames.contains("Location"));
+    assertTrue(response.isCommitted());
+  }
+
+  /**
+   * Test {@link ViewMessageServlet#doPost(HttpServletRequest, HttpServletResponse)}.
+   * <ul>
+   *   <li>Then {@link MockHttpServletResponse} (default constructor) HeaderNames size is one.</li>
+   * </ul>
+   * <p>
+   * Method under test: {@link ViewMessageServlet#doPost(HttpServletRequest, HttpServletResponse)}
+   */
+  @Test
+  @Category(MaintainedByDiffblue.class)
+  @MethodsUnderTest({"void ViewMessageServlet.doPost(HttpServletRequest, HttpServletResponse)"})
+  public void testDoPost_thenMockHttpServletResponseHeaderNamesSizeIsOne() throws IOException, ServletException {
+    // Arrange
+    ViewMessageServlet viewMessageServlet = new ViewMessageServlet();
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    MockHttpServletResponse response = new MockHttpServletResponse();
+
+    // Act
+    viewMessageServlet.doPost(request, response);
+
+    // Assert
+    Collection<String> headerNames = response.getHeaderNames();
+    assertEquals(1, headerNames.size());
+    assertTrue(headerNames instanceof Set);
+    assertEquals("login", response.getRedirectedUrl());
+    assertEquals(302, response.getStatus());
+    assertTrue(headerNames.contains("Location"));
+    assertTrue(response.isCommitted());
   }
 }
